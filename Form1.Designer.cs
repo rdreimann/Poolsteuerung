f688ef8324737664733137ee04c0b7d6a3be7b37
Phoenix Laptop <Phoenix Laptop@PHOENIXLAPTOP>
2021-06-22 19:58:45 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace Poolsteuerung
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_off = new System.Windows.Forms.Button();
+            this.btn_auto = new System.Windows.Forms.Button();
+            this.btn_timer = new System.Windows.Forms.Button();
+            this.timer_abfrage = new System.Windows.Forms.Timer(this.components);
+            this.timer_1h = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numer1 = new System.Windows.Forms.NumericUpDown();
+            this.Ausgang = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_off
+            // 
+            this.btn_off.BackColor = System.Drawing.Color.Lime;
+            this.btn_off.Location = new System.Drawing.Point(100, 150);
+            this.btn_off.Name = "btn_off";
+            this.btn_off.Size = new System.Drawing.Size(100, 100);
+            this.btn_off.TabIndex = 0;
+            this.btn_off.Text = "AUS";
+            this.btn_off.UseVisualStyleBackColor = false;
+            this.btn_off.Click += new System.EventHandler(this.btn_off_Click);
+            // 
+            // btn_auto
+            // 
+            this.btn_auto.BackColor = System.Drawing.Color.Red;
+            this.btn_auto.Location = new System.Drawing.Point(300, 150);
+            this.btn_auto.Name = "btn_auto";
+            this.btn_auto.Size = new System.Drawing.Size(100, 100);
+            this.btn_auto.TabIndex = 1;
+            this.btn_auto.Text = "AUTO";
+            this.btn_auto.UseVisualStyleBackColor = false;
+            this.btn_auto.Click += new System.EventHandler(this.btn_auto_Click);
+            // 
+            // btn_timer
+            // 
+            this.btn_timer.BackColor = System.Drawing.Color.Red;
+            this.btn_timer.Location = new System.Drawing.Point(500, 150);
+            this.btn_timer.Name = "btn_timer";
+            this.btn_timer.Size = new System.Drawing.Size(100, 100);
+            this.btn_timer.TabIndex = 2;
+            this.btn_timer.Text = "1h aktivieren";
+            this.btn_timer.UseVisualStyleBackColor = false;
+            this.btn_timer.Click += new System.EventHandler(this.btn_timer_Click);
+            // 
+            // timer_abfrage
+            // 
+            this.timer_abfrage.Enabled = true;
+            this.timer_abfrage.Interval = 5000;
+            this.timer_abfrage.Tick += new System.EventHandler(this.timer_abfrage_Tick);
+            // 
+            // timer_1h
+            // 
+            this.timer_1h.Interval = 1000;
+            this.timer_1h.Tick += new System.EventHandler(this.timer_1h_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(425, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(525, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(325, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // numer1
+            // 
+            this.numer1.Location = new System.Drawing.Point(50, 275);
+            this.numer1.Name = "numer1";
+            this.numer1.Size = new System.Drawing.Size(120, 20);
+            this.numer1.TabIndex = 10;
+            this.numer1.ValueChanged += new System.EventHandler(this.numer1_ValueChanged);
+            // 
+            // Ausgang
+            // 
+            this.Ausgang.AutoSize = true;
+            this.Ausgang.Location = new System.Drawing.Point(350, 275);
+            this.Ausgang.Name = "Ausgang";
+            this.Ausgang.Size = new System.Drawing.Size(80, 17);
+            this.Ausgang.TabIndex = 11;
+            this.Ausgang.Text = "checkBox1";
+            this.Ausgang.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 362);
+            this.Controls.Add(this.Ausgang);
+            this.Controls.Add(this.numer1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_timer);
+            this.Controls.Add(this.btn_auto);
+            this.Controls.Add(this.btn_off);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_off;
+        private System.Windows.Forms.Button btn_auto;
+        private System.Windows.Forms.Button btn_timer;
+        private System.Windows.Forms.Timer timer_abfrage;
+        private System.Windows.Forms.Timer timer_1h;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numer1;
+        private System.Windows.Forms.CheckBox Ausgang;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Device.Gpio;
+
+
+namespace Poolsteuerung
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static class Global
+        {
+            public static int Sektick = 10;
+            public static int Mintick = 1;
+            public static float labdec1 = 0;
+            public static float labdec2 = 0;
+            public static float labdec3 = 0;
+            public static float labdec4 = 0;
+            public static float labdec5 = 0;
+            public static float labdec6 = 0;
+        }
+
+        private void btn_off_Click(object sender, EventArgs e)
+        {
+            btn_off.BackColor = Color.Lime;
+            btn_auto.BackColor = Color.Red;
+            btn_timer.BackColor = Color.Red;
+            timer_1h.Enabled = false;
+            Global.Mintick = 01;
+            Global.Sektick = 60;
+            btn_timer.Text = "1h aktivieren";
+            Ausgang.Checked = false;
+        }
+
+        private void btn_auto_Click(object sender, EventArgs e)
+        {
+            btn_off.BackColor = Color.Red;
+            btn_auto.BackColor = Color.Lime;
+            btn_timer.BackColor = Color.Red;
+            Global.Mintick = 01;
+            Global.Sektick = 60;
+            btn_timer.Text = "1h aktivieren";
+            timer_1h.Enabled = false;
+        }
+
+        private void btn_timer_Click(object sender, EventArgs e)
+        {
+            btn_off.BackColor = Color.Red;
+            btn_auto.BackColor = Color.Red;
+            btn_timer.BackColor = Color.Lime;
+            timer_1h.Enabled = true;
+            Global.Mintick = 01;
+            Global.Sektick = 59;
+            Ausgang.Checked = true;
+           
+        }
+
+        private void timer_abfrage_Tick(object sender, EventArgs e)
+        {
+            //GPIO Ausgang vorbereiten
+            //int pin = 21;
+            //var controller = new GpioController();
+            //controller.OpenPin(pin, PinMode.Output);
+
+            float mittelwert;
+            //Werte durch die Zwischenspeicher schieben
+            Global.labdec5 = Global.labdec4;
+            Global.labdec4 = Global.labdec3;
+            Global.labdec3 = Global.labdec2;
+            Global.labdec2 = Global.labdec1;
+
+            //Durchschnitt berechnen
+            mittelwert = Global.labdec5 + Global.labdec4;
+            mittelwert = mittelwert + Global.labdec3;
+            mittelwert = mittelwert + Global.labdec2;
+            mittelwert = mittelwert + Global.labdec1;
+            Global.labdec6 = mittelwert / 5;
+
+            //Wert aus dem Nummernlabel ziehen
+            Global.labdec1 = (float)numer1.Value;
+
+            //Werte aus den Variablen ausgeben
+            int int_zwi = (int)Global.labdec1;
+            string str_zwi = int_zwi.ToString();
+            label1.Text = str_zwi;
+
+            int_zwi = (int)Global.labdec2;
+            str_zwi = int_zwi.ToString();
+            label2.Text = str_zwi;
+
+            int_zwi = (int)Global.labdec3;
+            str_zwi = int_zwi.ToString();
+            label3.Text = str_zwi;
+
+            int_zwi = (int)Global.labdec4;
+            str_zwi = int_zwi.ToString();
+            label4.Text = str_zwi;
+
+            int_zwi = (int)Global.labdec5;
+            str_zwi = int_zwi.ToString();
+            label5.Text = str_zwi;
+
+            int_zwi = (int)Global.labdec6;
+            str_zwi = int_zwi.ToString();
+            label6.Text = str_zwi;
+
+            // Ausgang schalten
+
+            if (btn_timer.BackColor == Color.Lime)
+            {
+                Ausgang.Checked = true;
+                //controller.Write(pin, PinValue.High);
+
+            }
+
+            else if (btn_auto.BackColor == Color.Lime & Global.labdec6 > 2.5 & Ausgang.Checked == false)
+            {
+                Ausgang.Checked = true;
+                //controller.Write(pin, PinValue.High);
+            }
+
+            else if (btn_auto.BackColor == Color.Lime & Global.labdec6 > 1.5 & Ausgang.Checked == true)
+            {
+                Ausgang.Checked = true;
+                //controller.Write(pin, PinValue.High);
+            }
+            else
+            {
+                Ausgang.Checked = false;
+                //controller.Write(pin, PinValue.Low);
+            }
+        }    
+
+        private void timer_1h_Tick(object sender, EventArgs e)
+        {
+            string Sekticker = Convert.ToString(Global.Sektick);
+            string Minticker = Convert.ToString(Global.Mintick);
+            string Zeitanzeige = Minticker += ":";
+            Zeitanzeige += Sekticker;
+            btn_timer.Text = Zeitanzeige;
+
+            if (Global.Mintick == 0)
+            {
+                if(Global.Sektick < 1)
+                {
+                    btn_timer.Text = "1h aktivieren";
+                    timer_1h.Enabled = false;
+                    btn_auto.PerformClick();
+                    Global.Sektick = 10;
+                    Global.Mintick = 1;
+                }
+            }
+
+            if (Global.Sektick < 1)
+            {
+                Global.Mintick--;
+                Global.Sektick = 60;
+            }
+
+            //Ausgang.Checked = true;
+
+            Global.Sektick = Global.Sektick - 1;
+
+        }
+
+        private void numer1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
